3da3f7812e70cdb2d303c0407d3a7d3127929fb7
drewparker <drewjparker91@gmail.com>
2020-08-17 13:44:56 -0700

    Create list
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>
    Co-authored-by: Alma  Frankenstein <a.q.frankenstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,18 @@ $(document).ready(function() {
     $("#userInput").hide();
     const item1 = $("input#item1").val().toUpperCase();
     const item2 = $("input#item2").val().toUpperCase();
-    const item3 = $("input#item3").val().toUpperCase();  
+    const item3 = $("input#item3").val().toUpperCase(); 
     let groceries = [item1,item2,item3];
     groceries.sort();
     groceries.forEach(function(grocery) {
-      $("#groceryList").append(" " + grocery + "!");
+      const ul = document.getElementById("groceryList");
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(grocery));
+      ul.appendChild(li);
+
     }); 
-            
+
     $("#userArray").show();    
   });
 });
+
