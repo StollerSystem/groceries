b1433a4b109b10662573d76a1669a0b12d11dfdb
drewparker <drewjparker91@gmail.com>
2020-08-17 13:53:34 -0700

    Go back to original loop state
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>
    Co-authored-by: Alma  Frankenstein <a.q.frankenstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,21 @@ $(document).ready(function() {
     const item1 = $("input#item1").val().toUpperCase();
     const item2 = $("input#item2").val().toUpperCase();
     const item3 = $("input#item3").val().toUpperCase(); 
+    //const li= "<li>" + grocery + "</li>" 
     let groceries = [item1,item2,item3];
     groceries.sort();
     groceries.forEach(function(grocery) {
-      const ul = document.getElementById("groceryList");
-      const li = document.createElement("li");
-      li.appendChild(document.createTextNode(grocery));
-      ul.appendChild(li);
-
+      let item = grocery
+    //   const ul = document.getElementById("groceryList");
+    //   const li = document.createElement("li");
+    //   li.appendChild(document.createTextNode(grocery));
+    //   ul.appendChild(li);
+      $("#groceryList").append("<li>" + item + "</li>");
+    // $("#groceryList").append(grocery);
+      $("#userArray").show();    
     }); 
 
-    $("#userArray").show();    
+   
   });
 });
 
